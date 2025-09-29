6030b08640953e5e630f33b0f61d39d9cc9c83c7
Ye_LIU <liuyefr@126.com>
2025-09-29 12:16:46 +0200

    submittedate

--- utils/HAL_search_api.py
+++ utils/HAL_search_api.py
@@ -57,7 +57,6 @@ def fetch_hal_articles(start_year=None, start_month=None, end_year=None, end_mon
 
     if keywords:
         fq.append("(" + " OR ".join([f'keyword_s:"{t}"' for t in keywords]) + ")") 
-    
 
     if languages:
         fq.append("(" + " OR ".join([f'language_s:"{t}"' for t in languages]) + ")") 
@@ -68,30 +67,54 @@ def fetch_hal_articles(start_year=None, start_month=None, end_year=None, end_mon
 
     #HAL（以及 Solr/Elasticsearch）检索时，如果字段是字符串 (_s 后缀通常是 string 类型)，直接用 [start TO end] 比较的是字典序，而不是时间
 
+
     if start_year is not None and start_month is not None:
-        start_date = f"{start_year}-{start_month:02d}-01 00:00:00"
+        start_date = f"{start_year}-{start_month:02d}"
+
+        # start_date = f"{start_year}-{start_month:02d}-01 00:00:00"
         # start_date = f"{start_year}-{start_month:02d}-01T00:00:00Z"
         # 你生成的是 ISO 8601 带 T 和 Z，而你存储的 modifiedDate_s 是空格分隔且没有 Z
     else:
         start_date = None
     
     end_day = calendar.monthrange(end_year, end_month)[1]#按月份决定最后一天是29/30/31
-    end_date = f"{end_year}-{end_month:02d}-{end_day:02d} 23:59:59"
+    end_date = f"{end_year}-{end_month:02d}"
 
+    # f"{end_year}-{end_month:02d}-{end_day:02d} 23:59:59"
     # end_date = f"{end_year}-{end_month:02d}-{end_day:02d}T23:59:59Z"
     
     if start_date:
-        fq.append(f'modifiedDate_s:[{start_date} TO {end_date}]')#publicationDate_s
+        fq.append(f'submittedDate_s:[{start_date} TO {end_date}]')#publicationDate_s,modifiedDate_s
         # print(f"PERIODE : {start_date} TO {end_date}")
 
     else:
-        fq.append(f'modifiedDate_s:[* TO {end_date}]')  # * 表示不限下限
+        fq.append(f'submittedDate_s:[* TO {end_date}]')  # * 表示不限下限
 
 
     if text:
         q = " AND ".join(text)  # 所有关键词都必须出现
     else:
         q="*:*"
+
+
+
+
+
+
+    # response = requests.get(url, params=params).json()
+
+    # if "response" in response:
+    #     docs = response["response"].get("docs", [])
+    #     if docs:
+    #         print(f"查询到 {len(docs)} 条结果")
+    #         for d in docs:
+    #             print(d)
+    #     else:
+    #         print("没有查询到结果")
+    # else:
+    #     print("查询返回异常")
+
+
 
     # CHECK: 
     #输入筛选条件
