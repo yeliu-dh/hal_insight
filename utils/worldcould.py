bd71067bde7deed8543126c4ec1997a111b51dd2
Ye_LIU <liuyefr@126.com>
2025-10-03 12:22:00 +0200

    Update worldcould.py

--- utils/worldcould.py
+++ utils/worldcould.py
@@ -31,7 +31,7 @@ def collect_texts_by_language(df, options, lang_col="language_s", langs=("en", "
     texts = {lang: [] for lang in langs}
 
     for col in options:
-        st.info(f"⚠️ Les {WC_MAP.get(col,"...")} sont manquants dans {df[col].isna().sum()}"
+        st.info(f"⚠️ Les {WC_MAP.get(col,'...')} sont manquants dans {df[col].isna().sum()}"
                 f" ({df[col].isna().sum()*100/len(df):.2f}%) articles!")
         if col not in df.columns:
             continue
