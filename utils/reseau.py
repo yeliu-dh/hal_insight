5faf0f8efa4788d0bee291821bfe054f17dbd4cf
Ye_LIU <liuyefr@126.com>
2025-10-15 15:05:35 +0200

    Update reseau.py

--- utils/reseau.py
+++ utils/reseau.py
@@ -196,9 +196,8 @@ def generate_network(df, options, stopwords, n=10, min_freq=2):
         src = edge['from']
         dst = edge['to']
         w = G[src][dst].get('weight', 1)
-
-        # edge['width'] = max(2, w*2)
-        edge['width'] = scale_size(w)
+        edge['width'] = max(10, w*10)
+        # edge['width'] = scale_size(w)
         edge['color'] = 'lightgray'
         edge['title'] = f"Cooccurrence : {int(w)}"
 
