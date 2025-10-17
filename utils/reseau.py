f245b313b33e7647b05f57e39061eb49dc304edd
Ye_LIU <liuyefr@126.com>
2025-10-17 12:59:45 +0200

    Update reseau.py

--- utils/reseau.py
+++ utils/reseau.py
@@ -118,8 +118,27 @@ def generate_network(df, options, stopwords, n=10, min_freq=2):
             filtered_edges.append((author, k))
             filtered_edge_weights[(author, k)] = w
 
-    # valid_keywords = {kw for _, kw in filtered_edges}    
-    st.write(filtered_edge_weights.items())
+    # valid_keywords = {kw for _, kw in filtered_edges}
+
+    for (a, k), w in filtered_edge_weights.items():
+        print(f"author:{a}")
+        print(f"keyword:{k}")
+        print(f"weight:{w}")
+        break 
+    
+
+    # st.write(filtered_edge_weights[0].items())
+    # [
+    #     0:[
+    #         0:[
+    #         0:"Philippe Lépinard"
+    #         1:"ludopédagogie"
+    #         ]
+    #     1:11
+    #     ],
+    # ...
+    # ]
+
 
     #==============================构建 NetworkX 图==================================
     # etworkX Graph 重复添加边会覆盖权重
