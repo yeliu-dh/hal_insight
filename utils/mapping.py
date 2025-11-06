14c194b4ebe025b13b9f92b5e78fc3e3e73bb964
Ye_LIU <liuyefr@126.com>
2025-11-06 21:20:13 +0100

    Update mapping.py

--- utils/mapping.py
+++ utils/mapping.py
@@ -89,24 +89,24 @@ def add_axe(df,axe_name='Axe'):
     return df
 
 
-def map_axe(df, col):
-    if col=="Axe":
-        # 定义映射字典
-        axe_map = {
-            "1": "Performances et responsabilités",
-            "2": "Société de services et services à la société",
-            "3": "Innovations transformations et résistances organisationnelles et sociétales",
-            "4": "Ouvrages pédagogiques"
-        }
+# def map_axe(df, col):
+#     if col=="Axe":
+#         # 定义映射字典
+#         axe_map = {
+#             "1": "Performances et responsabilités",
+#             "2": "Société de services et services à la société",
+#             "3": "Innovations transformations et résistances organisationnelles et sociétales",
+#             "4": "Ouvrages pédagogiques"
+#         }
 
-        # 拆分、映射、再合并
-        df['Axe'] = (
-            df['Axe']
-            .fillna('nan')
-            .astype(str)
-            .str.split(';')                  # 拆分多个值
-            .apply(lambda lst: [axe_map.get(x.strip(), x.strip()) for x in lst])  # 映射
-            .apply(lambda lst: ';'.join(lst))  # 再合并成字符串
-        )
-    return df
+#         # 拆分、映射、再合并
+#         df['Axe'] = (
+#             df['Axe']
+#             .fillna('nan')
+#             .astype(str)
+#             .str.split(';')                  # 拆分多个值
+#             .apply(lambda lst: [axe_map.get(x.strip(), x.strip()) for x in lst])  # 映射
+#             .apply(lambda lst: ';'.join(lst))  # 再合并成字符串
+#         )
+#     return df
 
